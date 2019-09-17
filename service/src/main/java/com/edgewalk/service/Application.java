0b813b74e63b2b1b746efbfa254661b3017f1a7f
romankasel <romankasel@10.10.34.74>
2019-09-16 18:05:36 -0700

    added mongo support

--- service/src/main/java/com/edgewalk/service/Application.java
+++ service/src/main/java/com/edgewalk/service/Application.java
@@ -1,8 +1,10 @@
 package com.edgewalk.service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
 
