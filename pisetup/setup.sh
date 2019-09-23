37353984e804b05d869d5f264ee372391a9a60e3
chris smith <csmith20@dons.usfca.edu>
2019-09-23 03:09:34 +0000

    fixed some issues

--- pisetup/setup.sh
+++ pisetup/setup.sh
@@ -1,34 +1,48 @@
 #!/bin/bash
+
+BASE=/etc/startup2/
+WPA=/etc/wpa_supplicant/wpa_supplicant.conf
+WIFI=${BASE}wifi
+GETIP=${BASE}getip
+MAIL=${BASE}mail_sender.py
+
 apt-get update
 apt-get install wpasupplicant
 apt-get install python3
 apt-get install wget
-apt-get install go
+apt-get install golang
+apt-get install vim
 
-wpa_passphrase $1 $2 >> /etc/wpa_supplicant/wpa_supplicant.conf
+if [ $1 -eq '' ] && [ $2 -eq '' ]
+then
+	echo "./setup <ssid> <password>"
+	exit 1
+else
+	wpa_passphrase $1 $2 >> $WPA
+fi
 
-mkdir /etc/startup
+mkdir $BASE
 
-touch /etc/startup/wifi
-echo "#!/bin/bash" >> /etc/startup/wifi
-echo sudo wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf >> /etc/startup/wifi
-echo sudo dhclient >> /etc/startup/
-chmod +x /etc/startup/wifi
-/etc/startup/wifi
+touch $WIFI
+echo "#!/bin/bash" >> $WIFI
+echo sudo wpa_supplicant -B -i wlan0 -c ${WPA} >> $WIFI
+echo sudo dhclient >> $WIFI
+chmod +x $WIFI
+$WIFI
 
-wget https://raw.githubusercontent.com/crsmith20/mail-sender/master/mail_sender.py --directory-prefix=/etc/startup/
-vi /etc/startup/mail_sender.py
+wget https://raw.githubusercontent.com/crsmith20/mail-sender/master/mail_sender.py --directory-prefix=$BASE
+vim $MAIL
 
-touch /etc/startup/getip
-echo "#!/bin/bash" >> /etc/startup/getip
-echo sudo python3 /etc/startup/mail_sender.py >> /etc/startup/getip
-chmod +x /etc/startup/getip
+touch $GETIP
+echo "#!/bin/bash" >> $GETIP
+echo sudo python3 ${MAIL} >> $GETIP
+chmod +x $GETIP
 
 echo Follow these following instructions carefully you will have 15 seconds to read the instructions
 echo
 echo Copy the following two lines
-echo @reboot /etc/startup/wifi&
-echo @reboot /etc/startup/getip&
+echo @reboot ${WIFI}'&'
+echo @reboot ${GETIP}'&'
 echo
 echo Once you the next command executes, it should ask you how you want to open the file
 echo Choose whatever you are most comfortable with
@@ -36,4 +50,4 @@ echo Once it opens please paste the 2 above lines into the file
 echo Then write and exit, this will automatically assign the tasks for next startup
 sleep 15
 crontab -e
-/etc/startup/getip
+$GETIP
