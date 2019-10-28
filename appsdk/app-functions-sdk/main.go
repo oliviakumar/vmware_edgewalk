6c310c8fd664045e05f4e3c9382fce4af6b8a9b4
Chris Smith <romankasel@Chriss-MacBook-Air.local>
2019-10-28 11:52:49 -0700

    made progress on app sdk

--- appsdk/app-functions-sdk/main.go
+++ appsdk/app-functions-sdk/main.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	"bytes"
 	"errors"
 	"fmt"
-	"image"
-	_ "image/jpeg"
-	_ "image/png"
 	"os"
-	"strings"
 
 	"github.com/edgexfoundry/app-functions-sdk-go/pkg/transforms"
-
-	"github.com/edgexfoundry/go-mod-core-contracts/models"
-
 	"github.com/edgexfoundry/app-functions-sdk-go/appcontext"
 	"github.com/edgexfoundry/app-functions-sdk-go/appsdk"
 )
@@ -25,13 +17,12 @@ const (
 func main() {
 
 	// 1) First thing to do is to create an instance of the EdgeX SDK, giving it a service key
-	edgexSdk := &appsdk.AppFunctionsSDK{
-		ServiceKey: serviceKey, // Key used by Registry (Aka Consul)
-	}
+	edgexSdk := &appsdk.AppFunctionsSDK{ServiceKey: serviceKey}
 
 	// 2) Next, we need to initialize the SDK
 	if err := edgexSdk.Initialize(); err != nil {
-		edgexSdk.LoggingClient.Error(fmt.Sprintf("SDK initialization failed: %v\n", err))
+		fmt.Println(err)
+		//edgexSdk.LoggingClient.Error(fmt.Sprintf("SDK initialization failed: %v\n", err))
 		os.Exit(-1)
 	}
 
@@ -41,38 +32,39 @@ func main() {
 
 	// 4) This is our pipeline configuration, the collection of functions to
 	// execute every time an event is triggered.
-	if err := edgexSdk.SetFunctionsPipeline(
-			transforms.NewFilter(deviceNames).FilterByDeviceName,
-			transforms.NewConversion().TransformJSON,
-			transforms.NewHttpSender("localhost:8080/edge/api", "application/json", false).HTTPPost,
-			printJSONToConsole
-		); err != nil {
-			edgexSdk.LoggingClient.Error(fmt.Sprintf("SDK SetPipeline failed: %v\n", err))
-			os.Exit(-1)
-		}
+	edgexSdk.SetFunctionsPipeline(
+		transforms.NewFilter(deviceNames).FilterByDeviceName,
+		transforms.NewConversion().TransformToJSON,
+		transforms.NewHTTPSender("localhost:8080/edge/api", "application/json", false).HTTPPost,
+		printJSONToConsole,
+	)
 
 	// 5) shows how to access the application's specific configuration settings.
-	appSettings := edgexSdk.ApplicationSettings()
-	if appSettings != nil {
-		appName, ok := appSettings["ApplicationName"]
-		if ok {
-			edgexSdk.LoggingClient.Info(fmt.Sprintf("%s now running...", appName))
-		} else {
-			edgexSdk.LoggingClient.Error("ApplicationName application setting not found")
-			os.Exit(-1)
-		}
-	} else {
-		edgexSdk.LoggingClient.Error("No application settings found")
+	//appSettings := edgexSdk.ApplicationSettings()
+	//if appSettings != nil {
+	//	appName, ok := appSettings["ApplicationName"]
+	//	if ok {
+	//		edgexSdk.LoggingClient.Info(fmt.Sprintf("%s now running...", appName))
+	//	} else {
+	//		edgexSdk.LoggingClient.Error("ApplicationName application setting not found")
+	//		os.Exit(-1)
+	//	}
+	//} else {
+	//	edgexSdk.LoggingClient.Error("No application settings found")
+	//	os.Exit(-1)
+	//}
+
+	// 6) Lastly, we'll go ahead and tell the SDK to "start" and begin listening for events to trigger the pipeline.
+	if err := edgexSdk.MakeItRun(); err != nil {
+		edgexSdk.LoggingClient.Error("MakeItRun returned error: ", err.Error())
 		os.Exit(-1)
 	}
 
-	// 6) Lastly, we'll go ahead and tell the SDK to "start" and begin listening for events to trigger the pipeline.
-	edgexSdk.MakeItRun()
-
+	os.Exit(0)
 }
 
 func printJSONToConsole(edgexcontext *appcontext.Context, params ...interface{}) (bool,interface{}) {
-	if len(params) < 1 { 
+	if len(params) < 1 {
 		// We didn't receive a result
 		return false, errors.New("No Data Received")
 	}
