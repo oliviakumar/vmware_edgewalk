5bef994d21f0e3e99cf59574a5533ab75dbfba87
Mushahid Hassan <smhassan@dons.usfca.edu>
2019-10-27 19:03:43 -0700

    Added testing and improved training

--- model-goface/main.go
+++ model-goface/main.go
@@ -5,26 +5,46 @@ import  (
     "log"
     "os"
     "io/ioutil"
+    "io"
+    "os/exec"
     "strings"
     "path/filepath"
     "github.com/Kagami/go-face"
 )
 
+//Struct for storing individual samples, id, and name
+type Train struct {
+    data []face.Descriptor
+    index int
+    name string
+}
+
+//Data directory for training the model
 var dataDir = "images"
-var count int = 0
+
+//Map that contains all the trained entries, makes it easier to retrieve info
+var model map[int]Train
 
 var samples []face.Descriptor
-var id []int32
-var labels []string
+var indSamples []face.Descriptor
+var tracker []int32
+var count int = 0
+var indCount int = 0
+
+var valid bool
+var found bool
+var approved bool
 
 //Changes the dataDir to given filename
 func changeDir(folder string) {
     dataDir = filepath.Join(dataDir, folder)
 }
 
-//Resets data directory
-func resetDir() {
+//Updates the variables and directory so that it can be resused for training a new person
+func update() {
     dataDir = "images"
+    indSamples = nil
+    indCount = indCount + 1
 }
 
 //Traverses through the given file and finds any ".jpg" to train the model with
@@ -44,19 +64,18 @@ func dirTraverse(file string, rec *face.Recognizer, name string) {
         }
         
         for _, f2 := range files {
-            newDir := filepath.Join(file, f2.Name())
-            fmt.Println(newDir)
-            
+            newDir := filepath.Join(file, f2.Name())            
             dirTraverse(newDir, rec, name)
         }
 
         //Updating count, which will be used as id
-        count = count + 1
-        fmt.Println("This is samples length: ", len(samples))
+        if (valid == true) {
+            populateDescriptor(name)
+        }
     //If the file given is a regular file
     case mode.IsRegular():
-        //Checks if the file is a jpg. Working on making this accept all images 
-        if (strings.HasSuffix(file, ".jpg") == true) {
+        //Checks if the file is a jpg or png
+        if (strings.HasSuffix(file, ".jpg") == true || strings.HasSuffix(file, ".jpeg") == true || strings.HasSuffix(file, ".png") == true) {
             faces, err := rec.RecognizeFile(file)
 
             if (err != nil) {
@@ -65,23 +84,132 @@ func dirTraverse(file string, rec *face.Recognizer, name string) {
 
             //Calling populateDescriptor
             if (len(faces) >= 1) {
-                populateDescriptor(rec, faces, name)
+                valid = true
+                samples = append(samples, faces[0].Descriptor)
+                indSamples = append(indSamples, faces[0].Descriptor)
+                tracker = append(tracker, int32(count))
+                count = count + 1
             }
         }
     }
 }
 
-//Populates the descriptor, id associated with the face, and name of the person
-func populateDescriptor(rec *face.Recognizer, faces []face.Face, name string) {
-    samples = append(samples, faces[0].Descriptor)
-    id = append(id, int32(count))
-    labels = append(labels, name)
+//Populates the Train struct, and stores it into a map
+func populateDescriptor(name string) {
+    var entry Train
+    entry.data = indSamples    
+    entry.index = indCount
+    entry.name = name
+
+    model[indCount] = entry
 }
 
+//Writes a shell script that opens an image path
+func WriteToFile(filename string, data string) error {
+    file, err := os.Create(filename)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    var newData string = "#!/bin/bash\n"
+    _, err = io.WriteString(file, newData)
+
+    newData = fmt.Sprintf("open %s", data)
+    _, err = io.WriteString(file, newData)
+    if err != nil {
+        return err
+    }
+
+    return file.Sync()
+}
+
+//Traverses through the training images folders to find the image match 
+func findPic(file string, rec *face.Recognizer, pic face.Descriptor) {
+    fi, err := os.Stat(file)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    
+    switch mode := fi.Mode(); {
+    //If the file given is a directory
+    case mode.IsDir():
+        files, err := ioutil.ReadDir(file)
+        if err != nil {
+            log.Fatal(err)
+        }
+        
+        for _, f2 := range files {
+            if (found) {
+                break
+            }
+            newDir := filepath.Join(file, f2.Name())
+            findPic(newDir, rec, pic)
+        }
+
+    //If the file given is a regular file
+    case mode.IsRegular():
+        if (strings.HasSuffix(file, ".jpg") == true || strings.HasSuffix(file, ".jpeg") == true || strings.HasSuffix(file, ".png") == true) {
+            tempFaces, err := rec.RecognizeFile(file)
+
+            if (err != nil) {
+                log.Fatalf("Can't recognize image")
+            }
+
+            //Finds the correct file and sends it's name to shell script
+            if (len(tempFaces) >= 1) {
+                if (pic == tempFaces[0].Descriptor) {
+                    fmt.Println("Trained image path: ", file)
+                    //Writing shell script open "file"
+                    WriteToFile("/Users/mushahidhassan/go/src/demo/open-pic.sh", file)
+                    
+                    fmt.Println("Opening image used to train...")
+                    //Executing shell script
+                    c := exec.Command("/Users/mushahidhassan/go/src/demo/open-pic.sh")
+                    if err2 := c.Run(); err2 != nil { 
+                        fmt.Println("Error: ", err2)
+                    }
+
+                    found = true
+                    break
+                }
+            }
+        }
+    }
+}
+
+func test(rec *face.Recognizer, picName string) {
+    dataDir = "images/testImages"
+    testPath := filepath.Join(dataDir, picName)
+    testPic, err := rec.RecognizeSingleFile(testPath)
+    
+    if err != nil {
+        log.Fatalf("Can't recognize: %v", err)
+    }
+    
+    if testPic == nil {
+        approved = false
+        log.Fatalf("Picture Match: %t", approved)
+    }
+    
+    picID := rec.Classify(testPic.Descriptor)
+    if picID < 0 {
+        log.Fatalf("Can't classify")
+    }
+
+    approved = true
+    fmt.Println("Picture Match: ", approved)
+    fmt.Println("ID that matches the pic: ", picID)
+
+    update()    
+    findPic(dataDir, rec, samples[picID])
+}
 
 func main () {
     fmt.Println("Facial Recognition System")
-    
+    model = make(map[int]Train)
+
     //Changing directory and training for Chris
     changeDir("Chris")
     rec, err := face.NewRecognizer(dataDir)
@@ -93,12 +221,11 @@ func main () {
     var name string = "Chris Smith"
     //Traversing through Chris directory
     dirTraverse(dataDir, rec, name)
-    rec.SetSamples(samples, id)
-    resetDir()
-
+    rec.SetSamples(samples, tracker)
+    update()
+    
     //Changing directory and training for Mushahid
     changeDir("Mushahid")
-    //Will work on generalizing the rec object
     rec, err = face.NewRecognizer(dataDir)
     if (err != nil) {
         log.Fatalf("Error opening directory.")
@@ -108,12 +235,11 @@ func main () {
     name = "Mushahid Hassan"
     //Traversing through Mushahid directory
     dirTraverse(dataDir, rec, name)
-    rec.SetSamples(samples, id)
-    resetDir()
+    rec.SetSamples(samples, tracker)
+    update()
 
     //Changing directory and training for Tanja
     changeDir("Tanja")
-    //Will work on generalizing the rec object
     rec, err = face.NewRecognizer(dataDir)
     if (err != nil) {
         log.Fatalf("Error opening directory.")
@@ -123,12 +249,11 @@ func main () {
     name = "Tanja Nuendel"
     //Traversing through Tanja directory
     dirTraverse(dataDir, rec, name)
-    rec.SetSamples(samples, id)
-    resetDir()
+    rec.SetSamples(samples, tracker)
+    update()
 
     //Changing directory and training for Olivia
     changeDir("Olivia")
-    //Will work on generalizing the rec object
     rec, err = face.NewRecognizer(dataDir)
     if (err != nil) {
         log.Fatalf("Error opening directory.")
@@ -138,32 +263,8 @@ func main () {
     name = "Olivia Kumar"
     //Traversing through Olivia directory
     dirTraverse(dataDir, rec, name)
-    rec.SetSamples(samples, id)
-    resetDir()
+    rec.SetSamples(samples, tracker)
+    update()
 
-    //Testing (Can use this as a basic test case for the model training)
-    //Uncomment to test using "mushTest.jpg"
-    // testMush := filepath.Join(dataDir, "mushTest.jpg")
-    // mush, err := rec.RecognizeSingleFile(testMush)
-    
-    // if err != nil {
-    //     log.Fatalf("Can't recognize: %v", err)
-    // }
-    
-    // if mush == nil {
-    //     log.Fatalf("Not a single face on the image")
-    // }
-    
-    // mushID := rec.Classify(mush.Descriptor)
-    // if mushID < 0 {
-    //     log.Fatalf("Can't classify")
-    // }
-
-    // fmt.Println("ID that matches the pic: ", mushID)
-    // for i := 0; i < len(id); i++ {
-    //     if (id[i] == int32(mushID)) {
-    //         fmt.Println("Name that matches the pic: ", labels[i])
-    //         break
-    //     }
-    // } 
+    test(rec, "test3.jpg")
 }
---
+++ model-goface/open-pic.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+open images/Mushahid/mush1.jpg
