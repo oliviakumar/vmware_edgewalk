0601672501bb91fb4bf7a1d24231d8edd04ec28f
Chris Smith <romankasel@Chriss-MacBook-Air.local>
2019-11-03 23:52:31 -0800

    fixed spacing

--- appsdk/app-functions-sdk/main.go
+++ appsdk/app-functions-sdk/main.go
@@ -28,9 +28,9 @@ type ImageData struct {
 
 type SendingData struct {
 	Identity  string `json:"identity"`
-    Accepted  bool `json:"accepted"`
+	Accepted  bool `json:"accepted"`
 	Location  string `json:"location"`
-    Entrytype string `json:"type"`
+	Entrytype string `json:"type"`
 	Device string `json:"device"`
 	Edgexid string `json:"edgexId"`
 	Imagepath string
