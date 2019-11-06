ca581f424279293ed8b176f293b36ee962c7d281
Olivia <oliviakumar@gmail.com>
2019-11-06 09:46:29 -0800

    updates to controller

--- service/src/main/java/com/edgewalk/service/controller/EdgeController.java
+++ service/src/main/java/com/edgewalk/service/controller/EdgeController.java
@@ -1,14 +1,21 @@
 package com.edgewalk.service.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.edgewalk.service.model.Response;
 import com.edgewalk.service.services.EdgeService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+// TODO adding autowiring for file service
+// @Autowired
 @RequestMapping("/edge")
 @RestController
 public class EdgeController {
@@ -18,6 +25,15 @@ public class EdgeController {
 	@PostMapping("/api")
 	public Response receive(@RequestBody Response response) {
 		return edgeService.processResponse(response);
+	}
+
+	// receives edgex id and multipart file
+	@GetMapping("/image")
+	@ResponseBody
+	public Response receive(@PathVariable String path, @RequestParam("file") MultipartFile file) {
+		// storageService.store(file);
+		// return edgeService.processResponse(response);
+		return null;
 	}
 
 	@PostMapping("/clear")
