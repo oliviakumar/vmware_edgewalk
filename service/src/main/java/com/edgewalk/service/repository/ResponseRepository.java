763a19c285eb53d81c22347dd24e0a35ff0e8a5d
Olivia <oliviakumar@gmail.com>
2019-11-09 12:22:01 -0800

    adding findByEdgexId method to ResponseRepository

--- service/src/main/java/com/edgewalk/service/repository/ResponseRepository.java
+++ service/src/main/java/com/edgewalk/service/repository/ResponseRepository.java
@@ -3,16 +3,18 @@ package com.edgewalk.service.repository;
 import java.util.Date;
 import java.util.List;
 
+import com.edgewalk.service.model.Response;
+
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import com.edgewalk.service.model.Response;
 
 @Repository
 public interface ResponseRepository extends MongoRepository<Response, String> {
 
 	@Query("{ '$and' : [ { 'attempted' : { '$lt' : ?1 } } , { 'attempted' : { '$gte' : ?0 } } ] }")
 	List<Response> findAllBetween(Date start, Date end, Sort sort);
+
+	Response findByEdgexId(String edgexId);
 }
