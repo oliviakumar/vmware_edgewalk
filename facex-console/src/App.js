a1df1119d3efb546c0e5d6cb24b8fe6fa292d1f8
Olivia <oliviakumar@gmail.com>
2019-11-27 14:35:44 -0800

    typing detail

--- facex-console/src/App.js
+++ facex-console/src/App.js
@@ -36,6 +36,69 @@ const ORG_API_URL = 'http://localhost:8080'
 const left = document.querySelector('.left');
 const container = document.querySelector('.container');
 
+const TypeWriter = function(txtElement, words, wait = 200) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+}
+
+// type method
+TypeWriter.prototype.type = function() {
+    const curr = this.wordIndex % this.words.length;
+    const fulltxt = this.words[curr];
+    console.log(fulltxt);
+
+    let typeSpeed = 75;
+
+    if(this.isDeleting) {
+        this.txt = fulltxt.substring(0, this.txt.length-1);
+        // typeSpeed /= 2;
+    } else {
+        this.txt = fulltxt.substring(0, this.txt.length+1);
+    }
+
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`
+
+    // let typeSpeed = 300;
+
+    if(this.isDeleting) {
+        typeSpeed /= 4;
+    }
+
+    if(!this.isDeleting && this.txt === fulltxt) {
+        // pause
+        typeSpeed = this.wait;
+
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.wordIndex++;
+
+        // pause
+        typeSpeed = 100;
+
+    }
+
+    setTimeout(() => this.type(), typeSpeed)
+}
+
+
+// init on DOM loadAsResource
+document.addEventListener('DOMContentLoaded', init);
+
+// init app
+function init() {
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+
+    // init twriter
+    new TypeWriter(txtElement, words, wait);
+}
 
 class App extends Component {
 
@@ -63,7 +126,7 @@ class App extends Component {
 
     return (
         <div id="app" style={{textAlign: 'center'}}>
-            <header class="masthead" width="100%">
+            <header className="masthead" width="100%">
 
             </header>
 
