1a8811355b5da33a0bf3a0438a5a0253df86654f
Olivia <oliviakumar@gmail.com>
2019-11-27 22:42:00 -0800

    team

--- facex-console/src/Bio/Bio.js
+++ facex-console/src/Bio/Bio.js
@@ -4,7 +4,8 @@ import './Bio.css';
 
 const bio = (props) => (
     <article className="Bio">
-        <h1>{props.name}</h1>
+        <img className="img" src={require(`../logos/${props.name}.jpg`)}></img>
+        <h1 className="name">{props.name}</h1>
         <div className="Info">
             <div className="Member">Member</div>
         </div>
--- facex-console/src/Team/Team.js
+++ facex-console/src/Team/Team.js
@@ -4,6 +4,10 @@ import Bio from '../Bio/Bio';
 // import FullPost from '../../components/FullPost/FullPost';
 // import NewPost from '../../components/NewPost/NewPost';
 import './Team.css';
+import tanja from '../logos/Tanja.jpg';
+import liv from '../logos/Olivia.jpg';
+import mushahid from '../logos/Mushahid.jpg';
+import chris from '../logos/Chris.jpg';
 
 class Team extends Component {
     state = {
@@ -12,12 +16,16 @@ class Team extends Component {
     render () {
         return (
             <div>
-                <section className="Bios">
-                    {this.state.names.map(name => {
-                        return <Bio name={name}/>;
-                    })
-                    }
+                <section style={{backgroundColor: 'black'}}>
+                    <h1 className="title"> TEAM EDGEWALK </h1>
+                    <section className="Bios">
+                        {this.state.names.map(name => {
+                            console.log(name + `.jpg`);
+                            return <Bio name={name} imgsrc={name}/>;
+                        })
+                        }
 
+                    </section>
                 </section>
 {
                 // <section>
