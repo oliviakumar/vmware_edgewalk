262540932531728f729078ec98b9d14f7e52e27f
Olivia <oliviakumar@gmail.com>
2019-11-28 20:53:31 -0800

    updating about, architecture, data flow

--- facex-console/src/About/About.jsx
+++ facex-console/src/About/About.jsx
@@ -1,31 +1,27 @@
 import React from "react";
 // import "./card-style.css";
-import rpi from "../rpi.jpg";
+import rpi from "../logos/rpi.jpg";
 import RoutedNav from '../RoutedNav/RoutedNav';
 import '../Team/Team.css';
+import InfoCard from "./InfoCard/InfoCard";
+import "./About.css";
+import { Row } from 'reactstrap'
+import {CardColumns} from "react-bootstrap";
+import dataflow from "../logos/data-flow.png";
+import arch from "../logos/architecture.png";
 
 const About = (props) => {
   // document.getElementById('veryimportant').style.display = "none";
   document.getElementById('banner').style.display = "none"
-  
-  return (
-    <div className='card text-center shadow Team'>
-    <RoutedNav/>
 
-      <div className='overflow'>
-        <img src={rpi} alt='Image 1' className='card-img-top' />
-      </div>
-      <div className='card-body text-dark'>
-        <h4 className='card-title'>{props.title}</h4>
-        <p className='card-text text-secondary'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-          distinctio maxime, sapiente temporibus cum quisquam totam voluptatum
-          asperiores. Necessitatibus, repellendus!
-        </p>
-        <a href='#' className='btn btn-outline-success'>
-          Go Anywhere
-        </a>
-      </div>
+  return (
+    <div>
+
+        <CardColumns className="p-5">
+            <InfoCard imgsrc={dataflow} />
+            <InfoCard imgsrc={arch} />
+            <InfoCard/>
+        </CardColumns>
     </div>
   );
 };
